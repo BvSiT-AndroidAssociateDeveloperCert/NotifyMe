ca12189bf3a7611e18f86ae7d9ecadb197bd0cc6
BvSiT <bvstraten1@gmail.com>
2019-12-10 18:19:01 +0100

    Solution NB Notification channel is not shown as category??

--- app/src/main/java/com/example/notifyme/MainActivity.java
+++ app/src/main/java/com/example/notifyme/MainActivity.java
@@ -1,21 +1,68 @@
 package com.example.notifyme;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button notify;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private static final int NOTIFICATION_ID = 0;
+    private Button button_notify;
+    private NotificationManager mNotifyManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        notify = findViewById(R.id.notify);
-
-
-
-
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(v->sendNotification());
+        createNotificationChannel(); //without this app crashes!
     }
 
-    
+    private NotificationCompat.Builder getNotificationBuilder(){
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this,NOTIFICATION_ID,notificationIntent,PendingIntent.FLAG_UPDATE_CURRENT); //!!
+
+        NotificationCompat.Builder notifyBuilder =
+                new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+            .setContentTitle("You've been notified!")
+            .setContentText("This is your notification text.")
+            .setSmallIcon(R.drawable.ic_stat_name)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            //backward compat <=7.1
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            ;
+        return  notifyBuilder;
+    }
+
+    private void createNotificationChannel(){
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT>=26){
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(PRIMARY_CHANNEL_ID,"Mascot Notification",NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void sendNotification(){
+        Notification notification = getNotificationBuilder().build();
+        mNotifyManager.notify(NOTIFICATION_ID,notification);
+    }
 }
